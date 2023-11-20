be2e44fe867f5dcd2c65525528dba04915e2471d
TARIK TUNC <tarik.tunc@ogr.iu.edu.tr>
2023-11-20 14:11:44 +0300

    update

--- Components/HorizantalItem/HorizontalItem.js
+++ Components/HorizantalItem/HorizontalItem.js
@@ -1,91 +1,93 @@
 "use client";
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect } from "react";
 import ItemCard from "./ItemCard";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
-export default function Home({ urlId, altName }) {
-    const [imageUrl, setImageUrl] = useState(
-        "https://image.tmdb.org/t/p/w500/"
-    );
-    const [movies, setMovies] = useState([]);
-    const [count, setCount] = useState({ prev: 0, current: 5 });
+export default function Home({ urlStatus }) {
+  const [movies, setMovies] = useState([]);
+  const [count, setCount] = useState({ prev: 0, current: 5 });
+  const languageSetting = useSelector(
+    (state) => state.languagesSetting.language
+  );
 
-    useEffect(() => {
-        const url = urlId;
-        const options = {
-            method: "GET",
-            headers: {
-                accept: "application/json",
-                Authorization:
-                    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYmM3ZjA5NzYwMDczZWVjZjFhY2Y2MWI1MmQxYmRhYyIsInN1YiI6IjY0N2YyYWVlMGUyOWEyMmJlMzI4ODQyNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.G74i4A-82vbKiB00myx0hSL5GGLPdcLO6yWFv_RIgg4",
-            },
-        };
-
-        fetch(url, options)
-            .then((res) => res.json())
-            .then((json) => setMovies(json.results))
-            .catch((err) => console.error("error:" + err));
-    }, []);
-
-    const handleClick = (e) => {
-        if (e.target.id === "prev") {
-            if (count.prev > 0) {
-                setCount({
-                    prev: (count.prev -= 1),
-                    current: (count.current -= 1),
-                });
-            }
-        }
-        if (e.target.id === "next") {
-            if (count.current < movies.length) {
-                setCount({
-                    prev: (count.prev += 1),
-                    current: (count.current += 1),
-                });
-            }
-        }
+  useEffect(() => {
+    const url = urlStatus;
+    const options = {
+      method: "GET",
+      url: `https://api.themoviedb.org/3/trending/movie/${url}?language=${languageSetting.toLowerCase()}-${languageSetting.toUpperCase()}`,
+      headers: {
+        accept: "application/json",
+        Authorization: process.env.API_KEY,
+      },
     };
 
-    return (
-        <div className="flex flex-col justify-center items-center my-10 px-10">
-            <div className="grid grid-cols-5">
-                {movies.slice(count.prev, count.current).map((movie) => (
-                    <div
-                        key={movie.id}
-                        className="transition-all duration-300 ease-in-out transform hover:scale-125">
-                        <ItemCard
-                            itemKey={movie.id}
-                            imageWidth="w-[150px]"
-                            url={imageUrl + movie.poster_path}
-                            altName={movie.id}
-                            rating={movie.vote_average}
-                            titleName={
-                                movie.title === undefined
-                                    ? movie.name
-                                    : movie.title
-                            }
-                            year={
-                                movie.release_date === undefined
-                                    ? movie.first_air_date
-                                    : movie.release_date
-                            }
-                        />
-                    </div>
-                ))}
-            </div>
-            <div className="flex justify-center">
-                <button
-                    id={"prev"}
-                    onClick={handleClick}
-                    className="transition-all duration-300 ease-in-out bg-blue-500 hover:bg-blue-700 text-white font-bold mx-5 py-3 px-10 rounded">
-                    Prev
-                </button>
-                <button
-                    id={"next"}
-                    onClick={handleClick}
-                    className="transition-all duration-300 ease-in-out bg-blue-500 hover:bg-blue-700 text-white font-bold mx-5 py-3 px-10 rounded">
-                    Next
-                </button>
-            </div>
-        </div>
-    );
+    axios
+      .request(options)
+      .then(function (response) {
+        setMovies(response.data.results);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, [languageSetting, urlStatus]);
+
+  const handleClick = (e) => {
+    if (e.target.id === "prev") {
+      if (count.prev > 0) {
+        setCount({
+          prev: (count.prev -= 1),
+          current: (count.current -= 1),
+        });
+      }
+    }
+    if (e.target.id === "next") {
+      if (count.current < movies.length) {
+        setCount({
+          prev: (count.prev += 1),
+          current: (count.current += 1),
+        });
+      }
+    }
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center my-10 px-10">
+      <div className="grid grid-cols-5">
+        {movies.slice(count.prev, count.current).map((movie) => (
+          <div
+            key={movie.id}
+            className="transition-all duration-300 ease-in-out transform hover:scale-125">
+            <ItemCard
+              itemKey={movie.id}
+              imageWidth="w-[150px]"
+              url={"https://image.tmdb.org/t/p/w500/" + movie.poster_path}
+              altName={movie.id}
+              rating={movie.vote_average}
+              titleName={movie.title === undefined ? movie.name : movie.title}
+              year={
+                movie.release_date === undefined
+                  ? movie.first_air_date
+                  : movie.release_date
+              }
+            />
+          </div>
+        ))}
+      </div>
+      <div className="flex justify-center">
+        <button
+          id={"prev"}
+          onClick={handleClick}
+          className="w-96 transition-all duration-300 ease-in-out bg-blue-500 hover:bg-blue-700 text-white font-bold mx-5  rounded">
+          Prev
+        </button>
+        <button
+          id={"next"}
+          onClick={handleClick}
+          className="w-96 transition-all duration-300 ease-in-out bg-blue-500 hover:bg-blue-700 text-white font-bold mx-5 rounded">
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
--- Components/VerticalItem/VerticalItem.js
+++ Components/VerticalItem/VerticalItem.js
@@ -1,51 +1,55 @@
 "use client";
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect } from "react";
 import ItemCard from "./ItemCard";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
-export default function Home({ urlId }) {
-    const [imageUrl, setImageUrl] = useState(
-        "https://image.tmdb.org/t/p/w500/"
-    );
-    const [movies, setMovies] = useState([]);
+export default function Home({ urlStatus }) {
+  const languageSetting = useSelector(
+    (state) => state.languagesSetting.language
+  );
+  const [movies, setMovies] = useState([]);
 
-    useEffect(() => {
-        const url = urlId;
-        const options = {
-            method: "GET",
-            headers: {
-                accept: "application/json",
-                Authorization:
-                    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYmM3ZjA5NzYwMDczZWVjZjFhY2Y2MWI1MmQxYmRhYyIsInN1YiI6IjY0N2YyYWVlMGUyOWEyMmJlMzI4ODQyNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.G74i4A-82vbKiB00myx0hSL5GGLPdcLO6yWFv_RIgg4",
-            },
-        };
+  useEffect(() => {
+    const url = urlStatus;
+    const options = {
+      method: "GET",
+      url: `https://api.themoviedb.org/3/trending/movie/${url}?language=${languageSetting.toLowerCase()}-${languageSetting.toUpperCase()}`,
+      headers: {
+        accept: "application/json",
+        Authorization: process.env.API_KEY,
+      },
+    };
 
-        fetch(url, options)
-            .then((res) => res.json())
-            .then((json) => setMovies(json.results))
-            .catch((err) => console.error("error:" + err));
-    }, []);
+    axios
+      .request(options)
+      .then(function (response) {
+        setMovies(response.data.results);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, [languageSetting, urlStatus]);
 
-    return (
-        <div className="flex flex-wrap max-w-screen-lg ">
-            {movies.map((movie) => (
-                <div key={movie.id}>
-                    <ItemCard
-                        itemKey={movie.id}
-                        imageWidth="w-[170px]"
-                        url={imageUrl + movie.poster_path}
-                        altName={movie.id}
-                        rating={movie.vote_average}
-                        titleName={
-                            movie.title === undefined ? movie.name : movie.title
-                        }
-                        year={
-                            movie.release_date === undefined
-                                ? movie.first_air_date
-                                : movie.release_date
-                        }
-                    />
-                </div>
-            ))}
+  return (
+    <div className="flex flex-wrap max-w-screen-lg ">
+      {movies.map((movie) => (
+        <div key={movie.id}>
+          <ItemCard
+            itemKey={movie.id}
+            imageWidth="w-[170px]"
+            url={"https://image.tmdb.org/t/p/w500/" + movie.poster_path}
+            altName={movie.id}
+            rating={movie.vote_average}
+            titleName={movie.title === undefined ? movie.name : movie.title}
+            year={
+              movie.release_date === undefined
+                ? movie.first_air_date
+                : movie.release_date
+            }
+          />
         </div>
-    );
+      ))}
+    </div>
+  );
 }
