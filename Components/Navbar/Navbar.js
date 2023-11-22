9751137c524990acdd09f8d7fa7a295f4612c3a2
TARIK TUNC <tarik.tunc@ogr.iu.edu.tr>
2023-11-22 16:24:10 +0300

    update

--- Components/Navbar/Navbar.js
+++ Components/Navbar/Navbar.js
@@ -5,58 +5,58 @@ import { setLanguage } from "@/stores/Slices/languageSettingSlice";
 import Country from "@/Components/Country/Country";
 
 export default function Navbar() {
-    const language = useSelector((state) => state.languagesSetting.language);
-    const buttonUrl = [
-        {
-            name: "Home",
-            id: "home",
-            url: "/",
-        },
-        {
-            name: "Movie",
-            id: "movie",
-            url: "/movie",
-        },
-        {
-            name: "Tv",
-            id: "tv",
-            url: "/tv",
-        },
-    ];
-    const dispatch = useDispatch();
-    const handleChange = (e) => {
-        dispatch(setLanguage(e.target.value));
-    };
+  const language = useSelector((state) => state.languagesSetting.language);
+  const buttonUrl = [
+    {
+      name: "Home",
+      id: "home",
+      url: "/",
+    },
+    {
+      name: "Movie",
+      id: "movie",
+      url: "/movie",
+    },
+    {
+      name: "Tv",
+      id: "tv",
+      url: "/tv",
+    },
+  ];
+  const dispatch = useDispatch();
+  const handleChange = (e) => {
+    dispatch(setLanguage(e.target.value));
+  };
 
-    return (
-        <div className="bg-[#221F1F] w-full h-14 flex items-center">
-            {/* Logo */}
-            <div className="text-white text-3xl w-full">LOGO</div>
+  return (
+    <div className="bg-[#221F1F] w-full h-14 flex items-center">
+      {/* Logo */}
+      <div className="text-white text-3xl w-full">LOGO</div>
 
-            {/* Buttons */}
-            <div className="flex justify-center items-center h-full w-full">
-                {buttonUrl.map((item) => {
-                    return (
-                        <div
-                            className=" w-full h-full items-center justify-center flex m-1"
-                            key={item.id}>
-                            <Link
-                                className="w-full h-full items-center justify-center flex text-white text-md hover:opacity-50"
-                                href={item.url}>
-                                {item.name}
-                            </Link>
-                        </div>
-                    );
-                })}
+      {/* Buttons */}
+      <div className="flex justify-center items-center h-full w-full">
+        {buttonUrl.map((item) => {
+          return (
+            <div
+              className=" w-full h-full items-center justify-center flex m-1"
+              key={item.id}>
+              <Link
+                className="w-full h-full items-center justify-center flex text-white text-md hover:opacity-50"
+                href={item.url}>
+                {item.name}
+              </Link>
             </div>
+          );
+        })}
+      </div>
 
-            {/* Search Bar */}
-            <div className="text-white text-md  flex justify-end  w-full ">
-                {language}
-                <div className="text-black">
-                    <Country handleCountryChange={handleChange} />
-                </div>
-            </div>
+      {/* Search Bar */}
+      <div className="text-white text-md  flex justify-end  w-full ">
+        {/* {language} */}
+        <div className="text-black">
+          {/* <Country handleCountryChange={handleChange} /> */}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
--- app/[slug]/layout.js
+++ app/[slug]/layout.js
@@ -1,13 +1,11 @@
 import Navbar from "@/Components/Navbar/Navbar";
-import Footer from "@/Components/Footer/Footer";
-import Head from "next/head";
 
 export default function RootLayout({ children }) {
-    return (
-        <>
-            <Navbar />
-            <div>{children}</div>
-            {/* <Footer /> */}
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <div>{children}</div>
+      {/* <Footer /> */}
+    </>
+  );
 }
