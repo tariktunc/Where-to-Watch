2179e90cfc020acce5f40b046781c39d55b16b4f
Tarık Tunç <tarik.tunc@ogr.iu.edu.tr>
2023-12-05 21:41:49 +0300

    Refactor loading logic in Poster component

--- Components/MovieProfile/Components/Poster/Poster.js
+++ Components/MovieProfile/Components/Poster/Poster.js
@@ -11,14 +11,14 @@ export default function Poster({ imageData, data, status }) {
   const [posterImage, setPosterImage] = useState(null);
   const [pathImage, setPathImage] = useState(null);
 
-  console.log(data);
-
   useEffect(() => {
     const moviePosterFilePath = data.poster_path;
     loadImage(moviePosterFilePath, "w500")
       .then((image) => {
         setPosterImage(image);
-        setPageLoading(false);
+        setTimeout(() => {
+          setPageLoading(false);
+        }, 200);
       })
       .catch((error) => {
         console.error("Görsel yüklenirken bir hata oluştu:", error.message);
@@ -37,7 +37,9 @@ export default function Poster({ imageData, data, status }) {
     loadImage(moviePathFilePath, "orginal")
       .then((image) => {
         setPathImage(image);
-        setPageLoading(false);
+        setTimeout(() => {
+          setPageLoading(false);
+        }, 200);
       })
       .catch((error) => {
         console.error("Görsel yüklenirken bir hata oluştu:", error.message);
@@ -67,9 +69,7 @@ export default function Poster({ imageData, data, status }) {
                 id="image_content"
                 className="block min-w-[300px] w-[300px] h-[450px] border-0 outline-none overflow-hidden">
                 {pageLoading ? (
-                  <div className="text-2xl h-screen flex justify-center items-center bg-blue-100">
-                    <Loading />
-                  </div>
+                  <Loading />
                 ) : (
                   <Image
                     className="rounded-md"
@@ -87,8 +87,7 @@ export default function Poster({ imageData, data, status }) {
                   className="text-white flex justify-center items-center h-10 w-full bg-blue-700 rounded-b-md hover:bg-blue-500 transition-all duration-100"
                   rel="icon"
                   href={data.homepage}>
-                  {" "}
-                  Play{" "}
+                  Play
                 </Link>
               )}
             </div>
@@ -97,59 +96,93 @@ export default function Poster({ imageData, data, status }) {
               id="ott_offer"
               className="flex flex-col justify-center h-[450px] text-white ">
               <section className="flex flex-col min-w-[400px]   items-start content-center box-border pl-[40px]">
-                <div
-                  id="title"
-                  className="flex justify-center items-center py-1">
-                  <h2 className="text-3xl font-bold">
-                    {status === "movie" ? data.title : data.name}
-                  </h2>
-                  <span id="release" className=" text-xl">
-                    (
-                    {setTimeout(() => {
-                      status === "movie"
-                        ? data.release_date.substring(0, 4)
-                        : data.first_air_date.substring(0, 4);
-                    }, 100)}
-                    )
-                  </span>
-                </div>
-
-                <ul id="genre" className="flex py-1">
-                  {/* {data.genres.map((genre, index) => (
-                    <li key={index} className="pr-1">
-                      {genre.name}
-                    </li>
-                  ))} */}
-                </ul>
-
-                <ul id="rating" className="py-1">
-                  <li id="charts">
-                    <div className="flex justify-center items-center">
-                      <Rating rating={data.vote_average} />
-                      <p className="font-italic text-sm pl-2">User Score</p>
-                    </div>
-                  </li>
-                </ul>
-
-                <div id="header_info" className="py-1">
-                  <h3 className="italic py-2 opacity-60">{data.tagline}</h3>
-                  <div>
-                    <p className="py-2">Overview</p>
-                    <p className="py-2 text-sm">{data.overview}</p>
+                {pageLoading ? (
+                  <div className="flex  p-1">
+                    <Loading />
                   </div>
-                </div>
+                ) : (
+                  <div
+                    id="title"
+                    className="flex justify-center items-center py-1">
+                    <h2 className="text-3xl font-bold">
+                      {status === "movie" ? data.title : data.name}
+                    </h2>
+                    <span id="release" className=" text-xl">
+                      (
+                      {status === "movie"
+                        ? data.release_date.substring(0, 4)
+                        : data.first_air_date.substring(0, 4)}
+                      )
+                    </span>
+                  </div>
+                )}
 
-                <ul id="language" className="py-2">
-                  {data.spoken_languages.map((language, index) => (
-                    <li key={index}> {language.english_name}</li>
-                  ))}
-                </ul>
+                {pageLoading ? (
+                  <div className="flex p-1">
+                    <Loading />
+                  </div>
+                ) : (
+                  <ul id="genre" className="flex py-1">
+                    {data.genres.map((genre, index) => (
+                      <li key={index} className="pr-1">
+                        {genre.name}
+                      </li>
+                    ))}
+                  </ul>
+                )}
 
-                <ul id="production_countries">
-                  {data.production_countries.map((country, index) => (
-                    <li key={index}> {country.name}</li>
-                  ))}
-                </ul>
+                {pageLoading ? (
+                  <div className="flex p-1">
+                    <Loading />
+                  </div>
+                ) : (
+                  <ul id="rating" className="py-1">
+                    <li id="charts">
+                      <div className="flex justify-center items-center">
+                        <Rating rating={data.vote_average} />
+                        <p className="font-italic text-sm pl-2">User Score</p>
+                      </div>
+                    </li>
+                  </ul>
+                )}
+
+                {pageLoading ? (
+                  <div className="flex p-1">
+                    <Loading />
+                  </div>
+                ) : (
+                  <div id="header_info" className="py-1">
+                    <h3 className="italic py-2 opacity-60">{data.tagline}</h3>
+                    <div>
+                      <p className="py-2">Overview</p>
+                      <p className="py-2 text-sm">{data.overview}</p>
+                    </div>
+                  </div>
+                )}
+
+                {pageLoading ? (
+                  <div className="flex  p-1">
+                    <Loading />
+                  </div>
+                ) : (
+                  <ul id="language" className="py-2">
+                    {data.spoken_languages.map((language, index) => (
+                      <li key={index}> {language.english_name}</li>
+                    ))}
+                  </ul>
+                )}
+
+                {pageLoading ? (
+                  <div className="flex p-1">
+                    <Loading />
+                  </div>
+                ) : (
+                  <ul id="production_countries">
+                    {data.production_countries.map((country, index) => (
+                      <li key={index}> {country.name}</li>
+                    ))}
+                  </ul>
+                )}
               </section>
             </div>
           </section>
